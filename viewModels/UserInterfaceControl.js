e23683321e1cb8115002ed05971032a44c060b9c
Najmul Hosain <developer.zeroone@gmail.com>
2021-08-01 09:04:55 +0600

    Update UserInterfaceControl.js

--- viewModels/UserInterfaceControl.js
+++ viewModels/UserInterfaceControl.js
@@ -1,4 +1,4 @@
-import { defined, DeveloperError, knockout } from 'cesium/Source/Cesium.js'
+import { defined, DeveloperError, knockout } from '../../../node_modules/cesium/Source/Cesium.js'
 
 var Knockout = knockout;
 /**
