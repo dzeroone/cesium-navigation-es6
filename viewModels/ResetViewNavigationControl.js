200da75f70d5e99481d64f7b3b036feaf5df1bcc
Najmul Hosain <developer.zeroone@gmail.com>
2021-08-01 09:04:30 +0600

    Update ResetViewNavigationControl.js

--- viewModels/ResetViewNavigationControl.js
+++ viewModels/ResetViewNavigationControl.js
@@ -1,5 +1,5 @@
 
-import { defined, Camera, Rectangle, Cartographic ,Math as CesiumMath} from 'cesium/Source/Cesium.js'
+import { defined, Camera, Rectangle, Cartographic ,Math as CesiumMath} from '../../../node_modules/cesium/Source/Cesium.js'
 import svgReset from '../svgPaths/svgReset'
 import NavigationControl from './NavigationControl'
 
